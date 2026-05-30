550ae9982174244f44bfed9e2aa4cbcae5f03e7c
Chrisa Aivalioti <chrisa@eaivaliotis.gr>
2026-05-30 18:33:36 +0300

    add 2 types of shop

--- src/components/customers/CustomerView.tsx
+++ src/components/customers/CustomerView.tsx
@@ -39,6 +39,8 @@ const SHOP_TYPE_LABELS: Record<string, string> = {
   public_service: 'Δημόσια Υπηρεσία',
   electronics_shop: 'Ηλεκτρονικό κατάστημα',
   agricultural_machinery: 'Γεωργικά μηχανήματα',
+  battery_oil: 'Μπαταρίες/Λιπαντικά',
+  bosch_car_service: 'Bosch Car Service',
   other: 'Άλλο',
 };
 
